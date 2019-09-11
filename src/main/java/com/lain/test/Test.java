f7df112a936d692e5fa406af20177e97a6a59a03
Administrator <Administrator@USER-20180315LA>
2019-09-11 15:30:14 +0800

    commit

--- src/main/java/com/lain/test/Test.java
+++ src/main/java/com/lain/test/Test.java
@@ -3,5 +3,7 @@ package com.lain.test;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("hello Git");
+		System.out.println("hello Git");
+		System.out.println("hello Git");
 	}
 }
