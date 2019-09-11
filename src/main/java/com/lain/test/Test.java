a7720bfc5a5184d1e364246d576103b32f35a024
IMChiLe <IMChiLe@DESKTOP-PAQ9GJ5>
2019-09-11 16:17:35 +0800

    by imchile

--- src/main/java/com/lain/test/Test.java
+++ src/main/java/com/lain/test/Test.java
@@ -3,12 +3,6 @@ package com.lain.test;
 public class Test {
 	public static void main(String[] args) {
 
-		System.out.println("helsadaslo Git");
-		System.out.println("helsadaslo Git");
-		System.out.println("helsadaslo Git");
-		System.out.println("²âÊÔ¸üĞÂ");
-
-		System.out.println("hello Git");
-		System.out.println("http");
+		System.out.println("hhhhh");
 	}
 }
