2694604e4c9639487b7f4bd9ee15a91ef435f493
Administrator <Administrator@USER-20180315LA>
2019-09-11 15:22:49 +0800

    commit

--- src/main/java/com/lain/test/Test.java
+++ src/main/java/com/lain/test/Test.java
@@ -2,16 +2,6 @@ package com.lain.test;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("helsadaslo Git");
-		System.out.println("helsadaslo Git");
-		System.out.println("helsadaslo Git");
-		System.out.println("helsadaslo Git");
-		System.out.println("helsadaslo Git");
-		System.out.println("helsadaslo Git");
-		System.out.println("helsadaslo Git");
-		System.out.println("helsadaslo Git");
-		System.out.println("helsadaslo Git");
-		System.out.println("helsadaslo Git");
-		System.out.println("helsadaslo Git");
+		System.out.println("hello Git");
 	}
 }
--- src/main/java/com/lain/util/AlarmTime.java
+++ src/main/java/com/lain/util/AlarmTime.java
@@ -6,7 +6,6 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Scanner;
 
 import com.lain.entity.DeviceAlarm;
 
